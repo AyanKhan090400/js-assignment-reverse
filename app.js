e430ae1d7f33ce9b8e7d3a048f6ac1edc55c9260
AYAN DEVELOPER <160521127+AyanKhan090400@users.noreply.github.com>
2025-06-29 05:11:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+alert("Enter a number in prompt the number print in reverse order without using loop");
+
+
+
+
+let arr = [];
+arr[0] = prompt("Enter a Number"); 
+arr[1] = prompt("Enter a Number"); 
+arr[2] = prompt("Enter a Number"); 
+arr[3] = prompt("Enter a Number"); 
+arr[4] = prompt("Enter a Number"); 
+arr[5] = prompt("Enter a Number"); 
+arr[6] = prompt("Enter a Number"); 
+arr[7] = prompt("Enter a Number"); 
+arr[8] = prompt("Enter a Number"); 
+
+let i = arr.length;
+
+document.writeln(arr[--i] + "</br>");
+document.writeln(arr[--i] + "</br>");
+document.writeln(arr[--i] + "</br>");
+document.writeln(arr[--i] + "</br>");
+document.writeln(arr[--i] + "</br>");
+document.writeln(arr[--i] + "</br>");
+document.writeln(arr[--i] + "</br>");
+document.writeln(arr[--i] + "</br>");
+document.writeln(arr[--i] + "</br>");
